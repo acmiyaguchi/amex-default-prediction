2cb9c740e47bae011bf1aad643d0891ea39d78fb
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2022-07-09 13:29:58 -0700

    Add initial commit with boilerplate for python code

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="amex_default_prediction",
+    version="0.1.0",
+    description="Utilities for amex default prediction challenge",
+    author="Anthony Miyaguchi",
+    author_email="acmiyaguchi@gmail.com",
+    url="https://github.com/acmiyaguchi/amex-default-prediction",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "click",
+        "tqdm",
+        "pyarrow",
+        "pyspark",
+        "torch",
+        "pytorch-lightning",
+        "torch-summary",
+        'importlib-metadata>=0.12;python_version<"3.8"',
+    ],
+)
