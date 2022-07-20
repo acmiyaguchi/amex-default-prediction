9e1ff2fd95ff91829233c42c83ac513092064f0f
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2022-07-19 23:07:23 -0700

    Refactor tests for updated fit_simple

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
 from pathlib import Path
+from uuid import uuid4
 
+import numpy as np
+import pandas as pd
 import pytest
+from pyspark.ml.functions import array_to_vector
+from pyspark.sql import functions as F
 
 from amex_default_prediction.utils import spark_session
 
@@ -15,3 +20,28 @@ def spark():
 @pytest.fixture(scope="session")
 def data_path():
     return Path(__file__).parent / "data"
+
+
+@pytest.fixture
+def synthetic_train_data_path(spark, tmp_path):
+    num_rows = 20
+    num_features = 3
+    pdf = pd.DataFrame(
+        dict(
+            customer_ID=[str(uuid4()) for _ in range(num_rows)],
+            label=np.random.randint(0, 2, num_rows),
+            features=[np.random.rand(num_features) for _ in range(num_rows)],
+        )
+    )
+    df = (
+        spark.createDataFrame(pdf)
+        .withColumn("features", array_to_vector("features"))
+        .withColumn("sample_id", F.crc32(F.col("customer_ID")) % 100)
+        .withColumn("most_recent", F.lit(True))
+        .repartition(1)
+    )
+    df.printSchema()
+    # our training procedure dumps out a data and pipeline directory
+    output = tmp_path / "test_data"
+    df.write.parquet((output / "data").as_posix())
+    yield output
--- tests/test_model_base.py
+++ tests/test_model_base.py
@@ -1,44 +1,23 @@
-from uuid import uuid4
-
-import numpy as np
-import pandas as pd
-import pytest
 from pyspark.ml.classification import LogisticRegression
-from pyspark.ml.functions import array_to_vector
 from pyspark.ml.tuning import CrossValidatorModel, ParamGridBuilder
 
 from amex_default_prediction.model.base import fit_simple
 
 
-@pytest.fixture
-def test_data_path(spark, tmp_path):
-    num_rows = 20
-    num_features = 3
-    pdf = pd.DataFrame(
-        dict(
-            customer_ID=[str(uuid4()) for _ in range(num_rows)],
-            target=np.random.randint(0, 2, num_rows),
-            features=[np.random.rand(num_features) for _ in range(num_rows)],
-        )
-    )
-    df = (
-        spark.createDataFrame(pdf)
-        .withColumn("features", array_to_vector("features"))
-        .repartition(1)
-    )
-    df.printSchema()
-    # our training procedure dumps out a data and pipeline directory
-    output = tmp_path / "test_data"
-    df.write.parquet((output / "data").as_posix())
-    yield output
-
-
-def test_logistic_regression_loads(tmp_path, spark, test_data_path):
+def test_logistic_regression_loads(tmp_path, spark, synthetic_train_data_path):
     model = LogisticRegression(
         maxIter=0, family="binomial", probabilityCol="probability"
     )
     grid = ParamGridBuilder().build()
     output = tmp_path / "model"
-    fit_simple(spark, model, grid, test_data_path, output, 0.8, 1)
+    fit_simple(
+        spark,
+        model,
+        grid,
+        synthetic_train_data_path,
+        output,
+        parallelism=1,
+        train_ratio=0.8,
+    )
     cv_model = CrossValidatorModel.read().load(output.as_posix())
     assert cv_model.avgMetrics[0]
