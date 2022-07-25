Reduce batch size and number of partitions, fix masking

amex_default_prediction/torch/data_module.py
@@ -144,6 +144,9 @@ def transform_into_transformer_pairs(df, length=4, partitions=32):
         .withColumn("tgt", slice_tgt("features_list", length))
         .withColumn("src_pos", slice_src("age_days_list", length))
         .withColumn("tgt_pos", slice_tgt("age_days_list", length))
+        # create padding mask before we actually pad src/tgt
+        .withColumn("k_src", F.size("src"))
+        .withColumn("k_tgt", F.size("tgt"))
         # pad src and tgt with arrays filled with zeroes
         .withColumn("dim", F.size(F.col("features_list")[0]))
         .withColumn(
@@ -154,21 +157,18 @@ def transform_into_transformer_pairs(df, length=4, partitions=32):
         )
         .withColumn("src_pos", pad_src("src_pos", F.lit(0), length))
         .withColumn("tgt_pos", pad_tgt("tgt_pos", F.lit(0), length))
-        # create padding mask
-        .withColumn("k_src", F.size("src"))
-        .withColumn("k_tgt", F.size("tgt"))
         .withColumn(
             "src_key_padding_mask",
             F.concat(
-                F.array_repeat(F.lit(True), F.lit(length) - F.col("k_src")),
-                F.array_repeat(F.lit(False), F.col("k_src")),
+                F.array_repeat(F.lit(1), F.lit(length) - F.col("k_src")),
+                F.array_repeat(F.lit(0), F.col("k_src")),
             ),
         )
         .withColumn(
             "tgt_key_padding_mask",
             F.concat(
-                F.array_repeat(F.lit(False), F.col("k_tgt")),
-                F.array_repeat(F.lit(True), F.lit(length) - F.col("k_tgt")),
+                F.array_repeat(F.lit(0), F.col("k_tgt")),
+                F.array_repeat(F.lit(1), F.lit(length) - F.col("k_tgt")),
             ),
         )
         # now lets flatten the src and tgt rows
@@ -184,7 +184,7 @@ def transform_into_transformer_pairs(df, length=4, partitions=32):
             "tgt_pos",
             F.lit(length).alias("subsequence_length"),
         )
-    )
+    ).repartition(partitions)
 
 
 class PetastormDataModule(pl.LightningDataModule):

amex_default_prediction/torch/net.py
@@ -44,7 +44,7 @@ class StrawmanNet(pl.LightningModule):
 
 
 # https://github.com/pytorch/examples/blob/main/word_language_model/model.py
-class PositionalEncoding(nn.Module):
+class PositionalEncoding(pl.LightningModule):
     r"""Inject some information about the relative or absolute position of the
         tokens in the sequence. The positional encodings have the same dimension
         as the embeddings, so that the two can be summed. Here, we use sine and
@@ -99,21 +99,24 @@ class TransformerModel(pl.LightningModule):
         self.transformer = nn.Transformer(d_model, dropout=dropout, **kwargs)
 
     def _generate_square_subsequent_mask(self, sz):
-        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
-        mask = (
-            mask.float()
-            .masked_fill(mask == 0, float("-inf"))
-            .masked_fill(mask == 1, float(0.0))
-        )
-        return mask
+        """Create a mask that masks starting from the right/
+
+        >>> torch.triu(torch.ones(sz, sz)) == 1
+        tensor([[ True,  True,  True],
+                [False,  True,  True],
+                [False, False,  True]])
+        """
+        return torch.triu(torch.ones(sz, sz)) == 1
 
     def _create_subsequence_mask(self, src, tgt):
         """Create the subsequence masks for src and target."""
         src_seq_len = src.shape[0]
         tgt_seq_len = tgt.shape[0]
 
-        tgt_mask = self._generate_square_subsequent_mask(tgt_seq_len)
-        src_mask = torch.zeros((src_seq_len, src_seq_len)).type(torch.bool)
+        tgt_mask = self._generate_square_subsequent_mask(tgt_seq_len).to(self.device)
+        src_mask = (
+            torch.zeros((src_seq_len, src_seq_len)).type(torch.bool).to(self.device)
+        )
         return src_mask, tgt_mask
 
     def forward(self, src, tgt, src_pos, tgt_pos, **kwargs):
@@ -152,8 +155,8 @@ class TransformerModel(pl.LightningModule):
             y,
             src_pos.transpose(0, 1),
             tgt_pos.transpose(0, 1),
-            src_key_padding_mask=src_key_padding_mask,
-            tgt_key_padding_mask=tgt_key_padding_mask,
+            src_key_padding_mask=src_key_padding_mask.type(torch.bool),
+            tgt_key_padding_mask=tgt_key_padding_mask.type(torch.bool),
         )
         return F.cross_entropy(z, y)
 

tests/test_torch.py
@@ -171,7 +171,12 @@ def test_petastorm_transformer_data_module_has_fields(
             == torch.Size([batch_size, 8 * subsequence_length])
         )
         # check subsequence length
-        assert batch["subsequence_length"][0] == subsequence_length
+        assert (
+            batch["subsequence_length"].cpu().detach().numpy()[0] == subsequence_length
+        )
+        assert batch["src_key_padding_mask"][0].type(torch.bool).dtype == torch.bool
+        assert batch["src_key_padding_mask"].cpu().detach().numpy().sum() > 0
+        assert batch["tgt_key_padding_mask"].cpu().detach().numpy().sum() > 0
         break
     assert batches == 1
 