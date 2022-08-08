767d849f237ae5d6cf3da69b8d1fdc0f1b398ab3
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2022-08-07 20:46:27 -0700

    Add sync script to move files to gcs

---
+++ scripts/sync.py
@@ -0,0 +1,35 @@
+#!/usr/env/bin python
+from pathlib import Path
+from subprocess import run
+
+import click
+
+root = Path(__file__).parent.parent
+
+PATHS = [
+    ("data/intermediate", "gs://amex-default-prediction-2022-data/intermediate"),
+]
+
+
+@click.group()
+def sync():
+    pass
+
+
+@sync.command()
+def up():
+    """Synchronize files from localhost to remote."""
+    for src, dst in PATHS:
+        run(f"gsutil -m rsync -r {src}/ {dst}/", shell=True, cwd=root)
+
+
+@sync.command()
+def down():
+    """Synchronize files from remote to localhost."""
+    for dst, src in PATHS:
+        Path(dst).mkdir(parents=True, exist_ok=True)
+        run(f"gsutil -m rsync -r {src}/ {dst}/", shell=True, cwd=root)
+
+
+if __name__ == "__main__":
+    sync()
